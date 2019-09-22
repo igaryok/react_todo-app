6e2cd3345b4c1f09b4bfe7a71debd5ecec122441
igaryok <igor.sobko@gmail.com>
2019-09-22 12:02:21 +0300

    Updates

---
+++ precache-manifest.d7c2c38125f698eba4a589cf77133ecd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933832fae875afc24bff4c4f5afaae6e",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "7191588b7b6ef8ef4e9c",
+    "url": "/react_todo-app/static/css/main.eff614b0.chunk.css"
+  },
+  {
+    "revision": "bcf0ecf1bbebd9e1ee95",
+    "url": "/react_todo-app/static/js/2.27f786d1.chunk.js"
+  },
+  {
+    "revision": "7191588b7b6ef8ef4e9c",
+    "url": "/react_todo-app/static/js/main.6dac320e.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  },
+  {
+    "revision": "8625837a60ad14e4715c2908647e1342",
+    "url": "/react_todo-app/static/media/HelveticaNeue-Thin.8625837a.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_todo-app/precache-manifest.36c0f0e73e4ba58e6c71741bdc35771c.js"
+  "/react_todo-app/precache-manifest.d7c2c38125f698eba4a589cf77133ecd.js"
 );
 
 self.addEventListener('message', (event) => {
